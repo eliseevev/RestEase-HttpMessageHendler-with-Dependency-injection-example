64026ce72b77473e5e960a9bb48e09e955cb08f5
Eliseev Eugene <eliseevmail@ya.ru>
2023-08-01 13:29:44 +0300

    Revert "Remove temp files"
    This reverts commit 1c1c960f5fe40f8e3abbd85a396d5f1c9816c090.

---
+++ Eliseev.RestEase.HttpMessageHendlerDependencyInjection/TestHeaderHttpClientHandler.cs
@@ -0,0 +1,20 @@
+﻿using Eliseev.RestEase.HttpMessageHendlerDIExample.Contracts;
+
+namespace Eliseev.RestEase.HttpMessageHendlerDIExample.Client
+{
+    internal class TestHeaderHttpClientHandler : DelegatingHandler
+    {
+        private readonly Incrementator incrementator;
+
+        public TestHeaderHttpClientHandler(Incrementator incrementator)
+        {
+            this.incrementator = incrementator;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add(HttpHeaders.TestHeader, incrementator.GetNext().ToString());
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
